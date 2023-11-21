6b6f040700e5fa726a188df5ce1dcfcb0ec367f2
Alexander Tarling <tarlinga@cardiff.ac.uk>
2023-11-21 21:16:20 +0000

    added new function which removed duplicates of value greater than 2

--- main.py
+++ main.py
@@ -10,10 +10,33 @@ def merge(num1, m, num2, n):
     num1.sort()
     return num1
 
+# remove if more than 2 duplicates, then sort
+def twoDuplicates(nums):
+    i=0
+    # for each value in the list
+    for n in nums:
+        if i<2 or n>nums[i-2]:
+            nums[i]=n
+            i+=1
+
+            # 1>-2 [1]
+            # 1>-1 [1,1]
+            # 1>1 [1,1]
+            # 2>1 [1,1,2]
+    return nums
 
 
 class Main(object):
     # merge two arrays and sort
-    array = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-    for i in range(6):
-        print(array[i])
+    # array = merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+    # for i in range(6):
+    #     print(array[i])
+
+    value = twoDuplicates([1,1,1,2])
+    for i in range(len(value)):
+        print(value[i])
+    print("number of values is " + str(len(value)))
+
+    # prints each value in list
+    for n in [1,1,1,2]:
+        print(n)
